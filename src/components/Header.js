748724905cd17eeb213d0c7068f1a36ff406ff1d
Baz Deas <bazdeas@gmail.com>
2018-09-06 10:13:38 -0400

    Add initial styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,20 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   padding: 2rem;
+
+  
+  .branding {
+    color: #434343;
+    font-weight: 700;
+    font-size: 28px;
+    letter-spacing: -0.42px;
+  }
 `
 
 export default () => {
   return(
     <Container>
-      <h2>Baz Deas</h2>
+      <h2 className="branding">Baz Deas</h2>
       <nav className="navigation">
         <a href="/">Hello</a>
         <a href="https://medium.com/@bazdeas">Writing</a>
