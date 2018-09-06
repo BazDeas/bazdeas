f363fa5d81b8280821717b9a60c1b0b57b7b8fe1
Baz Deas <bazdeas@gmail.com>
2018-09-06 13:21:02 -0400

    Add link to project in Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,56 +30,68 @@ const projects = [
 const Styling = styled.div`
 
   .project {
-    display: flex;
-    flex-wrap: nowrap;
-    list-style-type: none;
-    align-items: center;
     margin: 1.5rem 0;
+    list-style-type: none;
+    a {
+      display: flex;
+      flex-wrap: nowrap;
+      align-items: center;
+      text-decoration: none;
+      .branding {
+        width: 80px;
+        transform: translatex(-20px);
+      }
 
-    .branding {
-      width: 80px;
-      transform: translatex(-20px);
+      .title {
+        width: 70%;
+        .name {
+          font-weight: 700;
+          font-size: 20px;
+          color: #181818;
+          letter-spacing: -0.8px;
+        }
+        .description {
+          font-weight: 400;
+          font-size: 13px;
+          color: #A2A2A2;
+          letter-spacing: 0.15px;
+        }
+      }
+      &:hover {
+        .name {
+          color: ${colors.primary};
+        }
+      }
     }
 
-    .title {
-      width: 70%;
-      .name {
-        font-weight: 700;
-        font-size: 20px;
-        color: #181818;
-        letter-spacing: -0.8px;
-      }
-      .description {
-        font-weight: 400;
-        font-size: 13px;
-        color: #A2A2A2;
-        letter-spacing: 0.15px;
-      }
-    }
+
 
   }
 
   @media ${bp.large} {
     .project {
       margin: 2rem 0;
-      .branding {
-        width: 128px;
-        transform: translatex(-32px);
-      }
-      .title {
-        margin: 0 0 0 1rem;
-        .name {
-          display: inline;
-          font-weight: 700;
-          font-size: 40px;
+      a {
+        .branding {
+          width: 128px;
+          transform: translatex(-32px);
         }
-        .description {
-          display: inline;
-          margin: 0 0 0 1.5rem;
-          font-weight: 200;
-          font-size: 32px;
+        .title {
+          margin: 0 0 0 1rem;
+          .name {
+            display: inline;
+            font-weight: 700;
+            font-size: 40px;
+          }
+          .description {
+            display: inline;
+            margin: 0 0 0 1.5rem;
+            font-weight: 200;
+            font-size: 32px;
+          }
         }
       }
+
     }
 
   }
@@ -93,11 +105,13 @@ export default () => {
         <ul className="projects">
           {projects.map(({ name, description, branding, url }, i) =>
             <li key={i} className="project">
-              <Branding className="branding" logo={branding} />
-              <div className="title">
-                <h4 className="name">{name}</h4>
-                <h5 className="description">{description}</h5>
-              </div>
+              <a href={url}>
+                <Branding className="branding" logo={branding} />
+                <div className="title">
+                  <h4 className="name">{name}</h4>
+                  <h5 className="description">{description}</h5>
+                </div>
+              </a>
             </li>
           )}
         </ul>
