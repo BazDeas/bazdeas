b3cf0abdb4396ae417dff979cbe5d73353867e81
Baz Deas <bazdeas@gmail.com>
2018-09-06 10:16:55 -0400

    Add breakpoints system

---
+++ src/brand/breakpoints.js
@@ -0,0 +1,5 @@
+export default {
+  medium: 'screen and (min-width: 720px)',
+  large: 'screen and (min-width: 1024px)',
+  huge: 'screen and (min-width: 1600px)'
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import bp from '../brand/breakpoints'
 
 const Container = styled.div`
   padding: 2rem;
 
-  
+
   .branding {
-    color: #434343;
-    font-weight: 700;
     font-size: 28px;
     letter-spacing: -0.42px;
+
+    color: #434343;
+    font-weight: 700;
+  }
+
+  @media ${bp.large} {
+
+    .branding {
+      font-size: 36px;
+    }
+
   }
 `
 
